47f0233cae0ad0dc4cb84bc6fabf9862d7411c96
Kia Kazemi-Nia <kazemini@usc.edu>
2025-08-05 18:00:59 -0700

    Clarifiying y print

--- Source/evolver.py
+++ Source/evolver.py
@@ -722,7 +722,7 @@ class EA:
 
         # print the data after changing the encoding
         print("Genotype data: ", all_x, flush=True)
-        print(all_y, flush=True)
+        print("Y column: ", all_y, flush=True)
 
         # partition data based splits
         # Add stratify for classification?
